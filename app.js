4f0dd1b9146cb7ed0b0d6fee5abc23248f30c45e
luisaGonzales <luisagc711@gmail.com>
2017-09-28 03:04:20 -0500

    Cambiando a programación con clases

--- app.js
+++ app.js
@@ -2,58 +2,56 @@
 
 const API_KEY = "AIzaSyB6RQPxv-X6aojxx9IKh0Nc4twyqlMnitI";
 
-let app = {
-   result: {
-      videos: [],
-      selectedVideo: null,
-      searchTerm: "iPhone X"
-   },
+class App {
+      constructor() {
+            this.videos = [],
+                  this.selectedVideo = null,
+                  this.searchTerm = "iPhone X"
+      }
+      init() {
+            //app.videoSearch("iPhone");
+            this.youtubeSearch(this.searchTerm);
+      }
+      //<iframe className="embed-responsive-item" src={url}> </iframe>
+      getVideoList(videos) {
+            return videos.map((video, index) => {
+                  const imageUrl = video.snippet.thumbnails.default.url;
+                  const url = `https://www.youtube.com/embed/${video.id.videoId}`;
+                  return `<li> 
+                              <img class="media-object" src=${imageUrl} /> 
+                              <p> 
+                                 <iframe class="embed-responsive-item" src=${url}> </iframe>
+                              </p>
+                        </li>`;
+            });
+      }
+      youtubeSearch(searchTerm) {
+            console.log(searchTerm);
+            YTSearch({
+                        key: API_KEY,
+                        term: searchTerm
+                  },
+                  data => {
+                        this.videos = data;
+                        this.selectedVideo = data[0];
+                        this.searchTerm = searchTerm;
+                        let list = this.getVideoList(this.videos);
+                        console.log("lis: ", list);
+                        $("#root").append(list);
+                  });
+      }
+      videoSearch(searchTerm) {
+            jQuery.getJSON("list.json", data => {
+                  console.log("result", data.items);
+                  this.videos = data.items;
+                  this.selectedVideo = data.items[0];
+                  this.searchTerm = searchTerm;
+                  let list = this.getVideoList(this.videos);
+                  console.log("lis: ", list);
+                  $("#root").append(list);
+            });
+      }
+}
 
-   init: function() {
-      //app.videoSearch("iPhone");
-      app.youtubeSearch("iPhone X");
-   },
-   //<iframe className="embed-responsive-item" src={url}> </iframe>
-   getVideoList: function(videos) {
-      return videos.map((video, index) => {
-         const imageUrl = video.snippet.thumbnails.default.url;
-         const url = `https://www.youtube.com/embed/${video.id.videoId}`;
-         return `<li> 
-                     <img class="media-object" src=${imageUrl} /> 
-                     <p> 
-                        <iframe class="embed-responsive-item" src=${url}> </iframe>
-                     </p>
-               </li>`;
-      });
-   },
-   youtubeSearch: function(searchTerm) {
-      console.log(searchTerm);
-
-      YTSearch({ key: API_KEY, term: searchTerm }, data => {
-         console.log("result", data);
-         app.result = {
-            videos: data,
-            selectedVideo: data[0],
-            searchTerm: searchTerm
-         };
-         var list = app.getVideoList(app.result.videos);
-         console.log("lis: ", list);
-         $("#root").append(list);
-      });
-   },
-   videoSearch: function(searchTerm) {
-      jQuery.getJSON("list.json", data => {
-         console.log("result", data.items);
-         app.result = {
-            videos: data.items,
-            selectedVideo: data.items[0],
-            searchTerm: searchTerm
-         };
-         var list = app.getVideoList(app.result.videos);
-         console.log("lis: ", list);
-         $("#root").append(list);
-      });
-   }
-};
-
-$(document).ready(app.init);
+let app = new App();
+$(document).ready(this.init());
