7dfa5db01a9790715629e30aa28bfbc12cc62b48
luisaGonzales <luisagc711@gmail.com>
2017-09-28 03:27:53 -0500

    Cambia de lista

--- app.js
+++ app.js
@@ -4,23 +4,37 @@ const API_KEY = "AIzaSyB6RQPxv-X6aojxx9IKh0Nc4twyqlMnitI";
 
 class App {
       constructor() {
-            this.videos = [],
-                  this.selectedVideo = null,
-                  this.searchTerm = "iPhone X"
+            this.videos = [];
+            this.selectedVideo = null;
+            this.imagenes = undefined;
+            this.searchTerm = "Fito & Fitipaldis - Entre la espada y la pared";
+            this.inputBuscar = $("#inputBuscar");
+            this.btnBuscar = $("#btnBuscar");
+            this.reproducir = $("#playVideo");
+            this.informacion = $("#informacion");
+            this.lista = $("#root");
       }
       init() {
-            //app.videoSearch("iPhone");
             this.youtubeSearch(this.searchTerm);
+            this.inputBuscar.keyup((e) => {
+                  if (e.which == 13) {
+                        this.btnBuscar.click();
+                  }
+            });
+            this.btnBuscar.click((e) => {
+                  e.preventDefault();
+                  this.lista.empty();
+                  this.youtubeSearch(this.inputBuscar.val());
+            });
       }
-      //<iframe className="embed-responsive-item" src={url}> </iframe>
       getVideoList(videos) {
             return videos.map((video, index) => {
                   const imageUrl = video.snippet.thumbnails.default.url;
-                  const url = `https://www.youtube.com/embed/${video.id.videoId}`;
+                  
                   return `<li> 
-                              <img class="media-object" src=${imageUrl} /> 
+                              <img class="media-object" src=${imageUrl} id="${video.id.videoId}"/> 
                               <p> 
-                                 <iframe class="embed-responsive-item" src=${url}> </iframe>
+                                 <label>${video.snippet.title}</label>
                               </p>
                         </li>`;
             });
@@ -37,7 +51,14 @@ class App {
                         this.searchTerm = searchTerm;
                         let list = this.getVideoList(this.videos);
                         console.log("lis: ", list);
-                        $("#root").append(list);
+                        this.lista.append(list);
+                        let videoSeleccionado = this.videoSeleccionado(this.selectedVideo);
+                        this.imagenes = $("img");
+                        this.imagenes.click((e) => {
+                            this.lista.empty()
+                            this.youtubeSearch(e.target.id);
+                            console.log(e.target.id);
+                        });
                   });
       }
       videoSearch(searchTerm) {
@@ -51,7 +72,15 @@ class App {
                   $("#root").append(list);
             });
       }
+      videoSeleccionado(video){
+            const url = `https://www.youtube.com/embed/${video.id.videoId}`;
+            this.informacion.html(
+                  `<h2>${video.snippet.title}</h2>
+                  <h3>${video.snippet.description}</h3>`
+            );
+            return `<iframe class="embed-responsive-item" src=${url}></iframe>`;
+      }
 }
 
 let app = new App();
-$(document).ready(this.init());
+$(document).ready(app.init());
